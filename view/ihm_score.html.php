4f9d32d76b5830ed86f60999723fd1227ec37d4c
Camille Breuils <breuils.camille@gmail.com>
2018-01-20 22:29:31 +0100

    améliorations tableau + ajout graphique

--- view/ihm_score.html.php
+++ view/ihm_score.html.php
@@ -13,21 +13,26 @@
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-md-1">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-10">
                     <!-- affichage du tableau des scores -->
-                    <table border = 1>
+                    <table class="table table-bordered table-dark table-hover table-responsive table-striped"> <!-- table-striped permet d'afficher une ligne sur deux fond gris pour le tableau -->
                         <tbody>
+                        <div class="h1 text text-center text-primary">
+                        <?=
+                         'Tableau des scores';
+                        ?>
+                         </div>
                         <?php
                         $NbrCol = 10; //le nombre de tours
                         $NbrLigne = 5;//le nombre d'équipe
                         // -------------------------------
-                        $scores = array ('score1', 'score2', 'score3', 'score4', 'score5', 'score6', 'score7', 'score8', 'score9', 'score10');
+                        $scores = array ('18', '5', '14', '17', '28', '75', '158', '2', '1', '3');
 
                         // affichage de l'en-tête du tableau
                        ?>
-                        <td>
+                        <td class="text text-center">
                             <?php
                                 echo ' Équipe / Tours ';
                             ?>
@@ -35,37 +40,47 @@
                         <?php
                         for ($i=1; $i<=$NbrCol; $i++) {
                             ?>
-                            <td>
-                                <?php
-                                echo 'Tour n° '.$i;
+                            <td class="text text-center">
+                                <?=
+                                    'Tour '.$i;
                                 ?>
                             </td>
                             <?php
                         }
-                        // pour chaque équipe
+                        ?>
+                            <td>
+                                <?=
+                                    'Total';
+                                ?>
+                            </td>
+                        <?php
+                        // pour afficher les libellés de chaque équipe
                         for ($i=1; $i<=$NbrLigne; $i++)
                         {
                             ?>
                             <tr>
-
-                                <td>
-                                    <?php
-                                        echo 'Équipe n° '. $i;
+                                <td class="text text-center">
+                                    <?=
+                                        $i;
                                     ?>
                                 </td>
-
-
-                                <?php		// pour chaque tour (de l'équipe)
+                                <?php		// boucle pour chaque tour (de l'équipe)
                                 for ($j=1; $j<=$NbrCol; $j++)
                                 {
-                                    ?>		<td>
-                                    <?php			// -------------------------
+                                    ?>
+                                        <td class="table-light text text-dark text-center">
+                                    <?=			// -------------------------
                                     // score à afficher
-                                    echo ' nb'.$scores[$j-1].' ';
+                                        $scores[$j-1];
                                     // -------------------------
                                     ?>		</td>
                                 <?php	} // end for
                                 ?>
+                                <td class="text text-center">
+                                    <?=
+                                        count($scores);
+                                    ?>
+                                </td>
                             </tr>
                             <?php
                         } // end for
@@ -74,13 +89,65 @@
                     </table>
 
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-1">
                     <!-- -->
                 </div>
             </div>
         </div>
         <script src="js/ihm_score.js"></script>
-        <script src="bootstrap/js/jquery.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
+</html>
+
+
+
+
+<!-- AJOUT DES GRAPHIQUES -->
+
+<html>
+<head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="js/loader.js"></script>
+    <script type="text/javascript">
+
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages':['corechart']});
+
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function drawChart() {
+
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+                ['Équipe 1', 3],
+                ['Équipe 2', 1],
+                ['Équipe 3', 1],
+                ['Équipe 4', 1],
+                ['Équipe 5', 2]
+            ]);
+
+            // Set chart options
+            var options = {'title':'Répartition des points par équipe',
+                'width':400,
+                'height':300};
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+
+<body>
+<!--Div that will hold the pie chart-->
+<div id="chart_div"></div>
+</body>
 </html>
