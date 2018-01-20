00a3b2b386c644e2220b3a242710a79df1a0621d
allan <all.brochard@gmail.com>
2018-01-21 00:42:39 +0100

    add new object equipe

---
+++ equipe.php
@@ -0,0 +1,61 @@
+<?php
+class equipe
+{
+    public $score_tours = array();
+    public $equipe_name;
+    public $equipe_pass;
+
+    public function addScore($i, $score_tour){
+        $this->score_tours->add($i,$score_tour);
+    }
+
+    /**
+     * @return array
+     */
+    public function getScoreTours()
+    {
+        return $this->score_tours;
+    }
+
+    /**
+     * @param array $score_tours
+     */
+    public function setScoreTours($score_tours)
+    {
+        $this->score_tours = $score_tours;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEquipeName()
+    {
+        return $this->equipe_name;
+    }
+
+    /**
+     * @param mixed $equipe_name
+     */
+    public function setEquipeName($equipe_name)
+    {
+        $this->equipe_name = $equipe_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEquipePass()
+    {
+        return $this->equipe_pass;
+    }
+
+    /**
+     * @param mixed $equipe_pass
+     */
+    public function setEquipePass($equipe_pass)
+    {
+        $this->equipe_pass = $equipe_pass;
+    }
+
+
+}
