1f5c73732f0bcaa57557ae13970ac933f911cc51
Camille Breuils <breuils.camille@gmail.com>
2018-01-21 07:30:46 +0100

    css

--- view/ihm_score.html.php
+++ view/ihm_score.html.php
@@ -18,6 +18,10 @@
                 <div class="col-md-1">
                 </div>
                 <div class="col-md-10">
+
+                    <form>
+                        <input type="button" name="tour" id="tour" value="Fin de tour" />
+                    </form>
                     <!-- affichage du tableau des scores -->
                     <table class="table table-bordered table-dark table-hover table-responsive table-striped"> <!-- table-striped permet d'afficher une ligne sur deux fond gris pour le tableau -->
                         <tbody>
@@ -28,7 +32,6 @@
                          </div>
                         <?php
                         $NbrCol = 10; //le nombre de tours
-                        $NbrLigne = 5;//le nombre d'équipe
                         // -------------------------------
                         $scores = array ('18', '5', '14', '17', '28', '75', '158', '2', '1', '3');
 
@@ -56,33 +59,22 @@
                                 ?>
                             </td>
                         <?php
-                        // pour afficher les libellés de chaque équipe
-                        for ($i=1; $i<=$NbrLigne; $i++)
-                        {
-
-
-                            // récupération du nom de l'équipe pour affichage
-                           // $nomEquipe = new equipe();
-                           // $recupEquipe = equipe.getEquipeName();
-
-
 
                             ?>
                             <tr>
                                 <td class="text text-center">
-                                    <?= 'echo';
-                                    ?>
+                                    NSFW
                                 </td>
                                 <?php		// boucle pour chaque tour (de l'équipe)
                                 for ($j=1; $j<=$NbrCol; $j++)
                                 {
                                     ?>
-                                        <td class="table-light text text-dark text-center">
-                                    <?=			// -------------------------
+                                        <td class="table-light text text-dark text-center" name="col<?= $j ?>">
+                                    <?php		// -------------------------
                                     // score à afficher
-                                        $scores[$j-1];
+                                    //    $scores[$j-1];
                                     // -------------------------
-                                    ?>		</td>
+                                    ?>	</td>
                                 <?php	} // end for
                                 ?>
                                 <td class="text text-center">
@@ -91,9 +83,7 @@
                                     ?>
                                 </td>
                             </tr>
-                            <?php
-                        } // end for
-                        ?>
+
                         </tbody>
                     </table>
 
@@ -122,62 +112,17 @@
 
 <script>
     $('#test').onclick(function(e){
-        $equipe= new equipe();
-        $equipe->setEquipeName($("input[name=equipe]").val());
-        $equipe->setEquipePass($("input[name=password]").val());
+        $equipe1 = new equipe();
+        $equipe1->setEquipeName($("input[name=equipe]").val());
+        $equipe1->setEquipePass($("input[name=password]").val());
         var_dump(envoie_donnee_equipe($equipe->getEquipeName(),$equipe->getEquipePass()));
     });
+    $('#tour').onclick(function(){
+       $equipe1 = fin_de_tour($equipe1);
+       for($i=0; $1<10; $i++){
+           if($equipe1->score_tours[$i] != null ){
+                $('col<?= $i ?>').html($equipe1->score_tours[$i]);
+           }
+       }
+    });
 </script>
-
-
-
-
-
-<!-- AJOUT DES GRAPHIQUES -->
-
-<html>
-<head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="js/loader.js"></script>
-    <script type="text/javascript">
-
-        // Load the Visualization API and the corechart package.
-        google.charts.load('current', {'packages':['corechart']});
-
-        // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(drawChart);
-
-        // Callback that creates and populates a data table,
-        // instantiates the pie chart, passes in the data and
-        // draws it.
-        function drawChart() {
-
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-                ['Équipe 1', 3],
-                ['Équipe 2', 1],
-                ['Équipe 3', 1],
-                ['Équipe 4', 1],
-                ['Équipe 5', 2]
-            ]);
-
-            // Set chart options
-            var options = {'title':'Répartition des points par équipe',
-                'width':400,
-                'height':300};
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
-    </script>
-</head>
-
-<body>
-<!--Div that will hold the pie chart-->
-<div id="chart_div"></div>
-</body>
-</html>
