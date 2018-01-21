8781f5004874a5f71158102624e47100d787c004
allan <all.brochard@gmail.com>
2018-01-21 05:43:35 +0100

    add function fin_de_tour()

--- index.php
+++ index.php
@@ -2,47 +2,34 @@
 require_once("tool.php");
 
 $tour = 1;
-//initialisation equipe
-$equipe = new equipe();
 
-$equipe_name = "aylenTerabah";
-$pass= "azerty";
+function fin_de_tour(equipe $equipe){
+    $shell_cmd = escapeshellarg('python 24h-flechette/main.py');
+    $resultat = shell_exec($shell_cmd);
+    $parsed_json = json_decode($resultat);
 
-//$json = file_get_contents("data.json");
-//
-//$parsed_json = json_decode($json);
-//$case1 = $parsed_json->{'hit_1'}->{'case'};
-//$couleur1 = $parsed_json->{'hit_1'}->{'couleur'};
-//$case2 = $parsed_json->{'hit_2'}->{'case'};
-//$couleur2 = $parsed_json->{'hit_2'}->{'couleur'};
-//$case3 = $parsed_json->{'hit_3'}->{'case'};
-//$couleur3 = $parsed_json->{'hit_3'}->{'couleur'};
+    $case1 = $parsed_json->{'hit_1'}->{'case'};
+    $couleur1 = $parsed_json->{'hit_1'}->{'couleur'};
+    $case2 = $parsed_json->{'hit_2'}->{'case'};
+    $couleur2 = $parsed_json->{'hit_2'}->{'couleur'};
+    $case3 = $parsed_json->{'hit_3'}->{'case'};
+    $couleur3 = $parsed_json->{'hit_3'}->{'couleur'};
 
-$couleur1 ="";
-$couleur2 ="";
-$couleur3 ="";
+    $score1 = calcul_point($couleur1, $case1);
+    $score2 = calcul_point($couleur2, $case2);
+    $score3 = calcul_point($couleur3, $case3);
 
-$case1="";
-$case2="";
-$case3="";
+    $score_tour = calcul_score_tour($score1, $score2, $score3);
 
-$score1 = calcul_point($couleur1, $case1);
-$score2 = calcul_point($couleur2, $case2);
-$score3 = calcul_point($couleur3, $case3);
+    $outPut = envoi_donnee_score($equipe->getEquipeName(), $equipe->getEquipePass(), $score_tour);
 
-$score_tour = calcul_score_tour($score1, $score2, $score3);
+    $equipe->addScore($this->tour, $score_tour);
 
-//test d'envoi des score
-$outPut = envoi_donnee_score($equipe, $pass, $score_tour);
-var_dump($outPut);
-
-//test création équipe
-$equipe->setEquipeName($equipe_name);
-$equipe->setEquipePass($pass);
-$equipe->addScore($tour, $score_tour);
-
-if($tour <= 10){
-    $tour++;
-}else{
-    $tour = 1;
+    if($this->tour <= 10){
+        $this->tour++;
+    }else{
+        $this->tour = 1;
+    }
+    return $equipe;
 }
+
