406866c2bd1012a3f9224ca81add611e2d5935f0
allan <all.brochard@gmail.com>
2018-01-21 01:50:49 +0100

    change calcul

--- index.php
+++ index.php
@@ -18,7 +18,11 @@ $couleur2 = $parsed_json->{'hit_2'}->{'couleur'};
 $case3 = $parsed_json->{'hit_3'}->{'case'};
 $couleur3 = $parsed_json->{'hit_3'}->{'couleur'};
 
-$score_tour = calcul_score_tour($case1, $case2, $case3);
+$score1 = calcul_point($couleur1, $case1);
+$score2 = calcul_point($couleur2, $case2);
+$score3 = calcul_point($couleur3, $case3);
+
+$score_tour = calcul_score_tour($score1, $score2, $score3);
 
 //test d'envoi des score
 $outPut = envoi_donnee_score($equipe, $pass, $score_tour);
